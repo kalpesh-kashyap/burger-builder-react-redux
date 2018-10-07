df3ddfb1265c260f2f91bd053127870392a9fe07
DESKTOP-9POLD77\Kalpesh <kashyapkalpesh555@gmail.com>
2018-10-07 19:17:59 +0530

    source file for burger-builder

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    const transformIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey}/>
+            })
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    return (
+        <div className={Classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {transformIngredients}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    )
+};
+
+export default burger;
